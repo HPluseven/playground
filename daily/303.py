e9eabf654cdab71a7499e90d8368695f830dd402
seven <837709594@qq.com>
2021-03-01 20:55:35 +0800

    📦

--- daily/303.py
+++ daily/303.py
@@ -1,11 +1,14 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
+        self.sums = [0]
+        _sums = self.sums
+
+        for num in nums:
+            _sums.append(_sums[-1] + num)
 
     def sumRange(self, i: int, j: int) -> int:
-        return sum(self.nums[i:j+1])
-
+        return self.sum[j+1] - self.sums[i]
 
 
 # Your NumArray object will be instantiated and called as such:
