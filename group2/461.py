5b44c8ea4a7dde799317553e1655084cf8f7337f
seven <837709594@qq.com>
2021-03-02 22:13:28 +0800

    📦

--- group2/461.py
+++ group2/461.py
@@ -7,3 +7,14 @@ class Solution:
                 distance += 1
             xor = xor >> 1
         return distance
+
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
+        distance = 0
+        while xor:
+            distance += 1
+            xor = xor & (xor-1)
+
+        return distance
