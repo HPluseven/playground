aa4cc3dd2e5ef1a90c9a70682ff43cd24e9f1ee6
seven <837709594@qq.com>
2021-03-04 19:03:57 +0800

    📦

--- daily/354.py
+++ daily/354.py
@@ -1,26 +1,16 @@
 class Solution:
-    def maxEnvelopes(self, envelopes) -> int:
-    # def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        envs = sorted(envelopes, key=lambda x: x[0]*x[1])
-        n = len(envs)
-        groups = [0]
-        max_num = 0
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        if not envelopes:
+            return 0
+
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        n = len(envelopes)
+
+        f = [1] * len(envelopes)
 
         for i in range(n):
-            count = 1
-            last = envs[i]
-            for j in range(i+1, n):
-                cur = envs[j]
-                if last[0] < cur[0] and last[1] < cur[1]:
-                    last = cur
-                    count += 1
-            max_num = max(max_num, count)
-            groups.append(count)
-            if max_num > n-i:
-                break
+            for j in range(i):
+                if envelopes[j][1] < envelopes[i][1]:
+                    f[i] = max[f[i], f[j] + 1]
 
-        return max(groups)
-
-
-s = Solution()
-print(s.maxEnvelopes([[2, 100], [3, 200], [4, 300], [5, 500], [5, 400], [5, 250], [6, 370], [6, 360], [7, 380]]))
+        return max(f)
