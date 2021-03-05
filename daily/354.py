fdb68f84222926d269bd23e537dcf84c26fcac09
seven <837709594@qq.com>
2021-03-06 01:14:34 +0800

    📦

--- daily/354.py
+++ daily/354.py
@@ -11,6 +11,6 @@ class Solution:
         for i in range(n):
             for j in range(i):
                 if envelopes[j][1] < envelopes[i][1]:
-                    f[i] = max[f[i], f[j] + 1]
+                    f[i] = max(f[i], f[j] + 1)
 
         return max(f)
