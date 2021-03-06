13025b2f79a82ad70bb7b558d4ce33160163b7f7
seven <837709594@qq.com>
2021-03-06 10:34:43 +0800

    📦

---
+++ daily/503.py
@@ -0,0 +1,23 @@
+# 输入: [1,2,1]
+# 输出: [2,-1,2]
+# 解释: 第一个 1 的下一个更大的数是 2；
+# 数字 2 找不到下一个更大的数； 
+# 第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        max_ = max(nums)
+        n = len(nums)
+        ans = [-1] * n
+        arr = nums + nums[:]
+
+        for i in range(n):
+            for j in range(i+1,len(arr)):
+                if arr[i] == max_:
+                    break
+                if arr[j] > arr[i]:
+                    ans[i] = arr[j]
+                    break
+        return ans
